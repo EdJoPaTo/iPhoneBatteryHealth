7cd59f378fd9fb3ff9ac2f9c6849d3c831b527d6
Edgar Toll <edjopato@gmail.com>
2018-06-25 22:27:44 +0200

    feat: also generate png with white background

--- plot.sh
+++ plot.sh
@@ -3,4 +3,5 @@
 gnuplot stats.gnuplot
 
 inkscape -z -e "output.png" "output.svg" -w 1000 >/dev/null 2>/dev/null
+inkscape -z -e "output-white.png" "output.svg" -w 1000 -y 255 >/dev/null 2>/dev/null
 inkscape -z -A "output.pdf" "output.svg" -w 1000 2>/dev/null
