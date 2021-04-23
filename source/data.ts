5a9914c589f5e95aa693b5193bf2c1a14c618ac7
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2021-04-23 11:43:53 +0200

    feat: add typescript typings
    maybe useful for future use

---
+++ source/data.ts
@@ -0,0 +1,31 @@
+export interface Battery {
+  readonly age: string;
+  readonly device: string;
+  readonly health: Record<string, number>;
+  warningSince?: string;
+}
+
+export interface Person {
+  readonly _name: string;
+  readonly batteries: Battery[];
+}
+
+export interface BatteryWithPerson extends Battery {
+  readonly person: string;
+}
+
+export function allBatteries(persons: readonly Person[]): BatteryWithPerson[] {
+  const result: BatteryWithPerson[] = []
+
+  for (const p of persons) {
+    const name = p._name
+    const bat = p.batteries.map((b): BatteryWithPerson => ({
+      ...b,
+      person: name
+    }))
+
+    result.push(...bat)
+  }
+
+  return result
+}
